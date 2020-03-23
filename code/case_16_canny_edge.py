3ee3a8493883bba229d65fd7a2577c275558f627
Wai-Ngai <Wai-Ngai@hotmail.com>
2020-03-24 00:17:37 +0800

    second time commit

--- code/case_16_canny_edge.py
+++ code/case_16_canny_edge.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+
 '''
 Canny边缘
 
@@ -14,29 +15,25 @@ canny运算步骤：5步
 
 
 def canny_edge_demo(image):
-    blurred=cv.GaussianBlur(image,(3,3),0)
-    gray=cv.cvtColor(blurred,cv.COLOR_BGR2GRAY)
+    blurred = cv.GaussianBlur(image, (3, 3), 0)
+    gray = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
 
     # canny 要求非浮点数
-    grad_x=cv.Sobel(gray,cv.CV_16SC1,1,0)
-    grad_y=cv.Sobel(gray,cv.CV_16SC1,0,1)
+    grad_x = cv.Sobel(gray, cv.CV_16SC1, 1, 0)
+    grad_y = cv.Sobel(gray, cv.CV_16SC1, 0, 1)
 
     # 同时采用x，y方向的梯度提取边缘
-    edge_output=cv.Canny(grad_x,grad_y,50,150)
+    edge_output = cv.Canny(grad_x, grad_y, 50, 150)
 
     # 也可以直接采用原图进行提取边缘
     # edge_output=cv.Canny(gray,50,150)
 
-    cv.imshow("gray",gray)
-    cv.imshow("canny_edge",edge_output)
+    cv.imshow("gray", gray)
+    cv.imshow("canny_edge", edge_output)
 
-    dst=cv.bitwise_and(image,image,mask=edge_output)
+    dst = cv.bitwise_and(image, image, mask=edge_output)
 
-    cv.imshow("color_edge",dst)
-
-
-
-
+    cv.imshow("color_edge", dst)
 
 
 if __name__ == '__main__':
@@ -50,7 +47,6 @@ if __name__ == '__main__':
     cv.imshow("lena", img)
 
     canny_edge_demo(img)
-
 
     # 等待键盘输入
     cv.waitKey(0)
