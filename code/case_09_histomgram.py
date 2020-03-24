62de1d4be342eafdf881765f0036cccb06db79a5
Wai-Ngai <Wai-Ngai@hotmail.com>
2020-03-25 00:19:03 +0800

    second time commit

--- code/case_09_histomgram.py
+++ code/case_09_histomgram.py
@@ -3,11 +3,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 '''
-图像直方图
+图像直方图：统计每个像素点的个数
 
-一维
+    一维
+        cv2.calcHist(images,channels,mask,histSize,ranges)
 
-二维
+            - images： 原图像图像格式为 uint8 或 ﬂoat32。当传入函数时应用中括号 [] 括来例如[img]
+            - channels：同样用中括号括来它会告函数我们统幅图 像的直方图。
+               - 如果入图像是灰度图它的值就是 [0]
+               - 如果是彩色图像的传入的参数可以是 [0] [1] [2] 它们分别对应着 BGR。
+            - mask：掩模图像。统整幅图像的直方图就把它为 None。但是如 果你想统图像某一分的直方图的你就制作一个掩模图像并 使用它。
+            - histSize：BIN 的数目。也应用中括号括来
+            - ranges：像素值范围常为 [0256]
+    
+    
+    
+    二维
 
 '''
 
